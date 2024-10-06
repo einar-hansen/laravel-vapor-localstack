4ec558cfa116376f2e976f25f45f9df29a955d32
Einar Hansen <49709354+einar-hansen@users.noreply.github.com>
2024-10-06 11:39:55 +0200

    Add node

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+export default ({ mode }) => {
+    // Load app-level env vars to node-level env vars.
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+        resolve: {
+            alias: {
+                "@": "/resources/js",
+            },
+        },
+        server: {
+            host: "0.0.0.0",
+            port: process.env.VITE_FORWARD_PORT ?? 5174,
+            hmr: {
+                host: "localhost",
+            },
+        },
+    });
+};
